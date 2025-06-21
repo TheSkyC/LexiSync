c79a60217348c231919fa77cf1eef21b0b80109a
TheSkyC <0x4fe6@gmail.com>
2025-06-21 20:32:30 +0800

    fix: fix comments display

--- app.py
+++ app.py
@@ -1640,6 +1640,7 @@ class OverwatchLocalizerApp:
 
         self._update_placeholder_highlights()
 
+        self.comment_edit_text.delete("1.0", tk.END)
         if self.is_po_mode:
             comment_to_display = getattr(ts_obj, 'source_comment', ts_obj.comment)
             self.comment_edit_text.insert("1.0", comment_to_display)
