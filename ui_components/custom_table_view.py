2f0b8501d71ef308b1cbbd3df8ff23d071a435da
TheSkyC <0x4fe6@gmail.com>
2025-06-28 00:21:45 +0800

    Optimize list scrolling performance

--- ui_components/custom_table_view.py
+++ ui_components/custom_table_view.py
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from PySide6.QtWidgets import QTableView
-from PySide6.QtCore import Qt, QPoint
+from PySide6.QtCore import Qt, QPoint, QTimer
 
 
 class CustomTableView(QTableView):
@@ -12,13 +12,51 @@ class CustomTableView(QTableView):
         self.is_dragging = False
         self.press_pos = QPoint()
 
+        self._is_scrollbar_dragging = False
+        self._dx_accumulator = 0
+        self._dy_accumulator = 0
+
+        self._scroll_throttle_timer = QTimer(self)
+        self._scroll_throttle_timer.setInterval(20)
+        self._scroll_throttle_timer.setSingleShot(True)
+        self._scroll_throttle_timer.timeout.connect(self._perform_throttled_scroll)
+        scrollbar = self.verticalScrollBar()
+        scrollbar.sliderPressed.connect(self._on_slider_pressed)
+        scrollbar.sliderReleased.connect(self._on_slider_released)
+
+    def _perform_throttled_scroll(self):
+        if self._dx_accumulator != 0 or self._dy_accumulator != 0:
+            super().scrollContentsBy(self._dx_accumulator, self._dy_accumulator)
+            self._dx_accumulator = 0
+            self._dy_accumulator = 0
+
+    def _on_slider_pressed(self):
+        self._is_scrollbar_dragging = True
+        self._dx_accumulator = 0
+        self._dy_accumulator = 0
+
+    def _on_slider_released(self):
+        self._is_scrollbar_dragging = False
+        if self._scroll_throttle_timer.isActive():
+            self._scroll_throttle_timer.stop()
+        self._perform_throttled_scroll()
+
+    def scrollContentsBy(self, dx, dy):
+        if self._is_scrollbar_dragging:
+            self._dx_accumulator += dx
+            self._dy_accumulator += dy
+            if not self._scroll_throttle_timer.isActive():
+                self._scroll_throttle_timer.start()
+        else:
+            super().scrollContentsBy(dx, dy)
+
     def mousePressEvent(self, event):
         self.is_dragging = False
         self.press_pos = event.pos()
 
         index = self.indexAt(event.pos())
         if index.isValid():
-            ts_obj = self.model().data(index, Qt.UserRole)
+            ts_obj = self.model().data(index, Qt.ItemDataRole.UserRole)
             if ts_obj and self.app and self.app.current_focused_ts_id != ts_obj.id:
                 self.app.current_focused_ts_id = ts_obj.id
                 self.viewport().update()
