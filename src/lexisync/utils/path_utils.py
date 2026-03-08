0087f65a8a90503dd54216a4e0cb9067192d38a7
TheSkyC <0x4fe6@gmail.com>
2026-03-08 13:38:32 +0800

    refactor: move source code to src/lexisync directory

--- src/lexisync/utils/path_utils.py
+++ src/lexisync/utils/path_utils.py
@@ -11,7 +11,7 @@ def get_resource_path(relative_path: str) -> str:
     try:
         base_path = sys._MEIPASS
     except Exception:
-        base_path = os.path.abspath(".")
+        base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
     return os.path.join(base_path, relative_path)
 
